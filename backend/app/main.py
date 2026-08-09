d25e4a0b184cd02ebc44d930e36941f00b7d254f
lakshya-makwana <m2lakshya@icloud.com>
2026-08-09 15:22:31 +0530

    Complete JWT authentication

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.auth import router as auth_router
+from app.api.users import router as users_router
 
 from app.database.base import Base
 from app.database.database import engine
@@ -13,7 +14,9 @@ app = FastAPI(
     title="AI Interview Preparation API",
     version="1.0.0",
 )
+
 app.include_router(auth_router)
+app.include_router(users_router)
 
 @app.get("/")
 def home():
